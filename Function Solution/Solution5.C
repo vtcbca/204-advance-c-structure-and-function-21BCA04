ccf9cf9635807df9d40cf0b2116f4d3c4a9b64b9
KAMLESH KUMAWAT <96828566+21BCA04@users.noreply.github.com>
2022-04-04 10:35:44 +0530

    Solution 5 half uploaded....!

---
+++ Function Solution/Solution5.C
@@ -0,0 +1,13 @@
+/* menu driven program to perform
+ using differnet function catagory
+ 1) prime
+ 2) palimdrome
+ 3) reverce number
+ 4) even number series till N
+ 5)
+ 6) exit */
+#include<stdio.h>
+#include<conio.h>
+void sum(int,int,int);// function declartion
+void main()
+{
